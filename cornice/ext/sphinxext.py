9cc98c429b938edff0e2d3b6a46c56af30cb446d
Nejc Zupan <nejc.zupan@gmail.com>
2016-12-13 00:36:06 +0100

    exec directive and py3 support

--- cornice/ext/sphinxext.py
+++ cornice/ext/sphinxext.py
@@ -160,6 +160,8 @@ class ServiceDirective(Directive):
 
                             # find attr in SQLAlchemy table definition and
                             # check that it has view permission
+                            if not getattr(args['klass'], 'model', None):
+                                continue
                             columns = args['klass'].model.__table__.columns
                             if column_name in columns.keys():
                                 info = getattr(columns[column_name], 'info', None)
@@ -340,7 +342,43 @@ def rst2node(data):
         return par
 
 
+from os.path import basename
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from sphinx.util.compat import Directive
+from docutils import statemachine
+
+
+class ExecDirective(Directive):
+    """Execute the specified python code and insert the output into the document."""
+    has_content = True
+
+    def run(self):
+        """Do it."""
+        oldStdout, sys.stdout = sys.stdout, StringIO()
+
+        tab_width = self.options.get('tab-width', self.state.document.settings.tab_width)
+        source = self.state_machine.input_lines.source(self.lineno - self.state_machine.input_offset - 1)
+
+        try:
+            exec('\n'.join(self.content))
+            text = sys.stdout.getvalue()
+            lines = statemachine.string2lines(text, tab_width, convert_whitespace=True)
+            self.state_machine.insert_input(lines, source)
+            return []
+        except Exception:
+            return [nodes.error(None, nodes.paragraph(text = "Unable to execute python code at %s:%d:" % (basename(source), self.lineno)), nodes.paragraph(text = str(sys.exc_info()[1])))]
+        finally:
+            sys.stdout = oldStdout
+
+
 def setup(app):
     """Hook the directives when Sphinx ask for it."""
     app.add_directive('services', ServiceDirective)  # deprecated
     app.add_directive('cornice-autodoc', ServiceDirective)
+    app.add_directive('exec', ExecDirective)
+
